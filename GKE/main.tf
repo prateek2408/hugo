86aef28a2f337f8f3d3f4b2dd0879ae2d0761b82
Prateek Khushalani <pkhushalani@google.com>
2021-09-16 13:54:54 +0530

    Modularising GKE module

--- GKE/main.tf
+++ GKE/main.tf
@@ -1,8 +1,3 @@
-resource "google_service_account" "default" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
-}
-
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
