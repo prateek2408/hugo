1ef20aeefe784f6996d54837469c61dd20c2350b
Prateek Khushalani <pkhushalani@google.com>
2021-09-16 16:40:11 +0530

    each used instead of entire name

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "GKE" {
   #Create multiple K8S clusters using for_each
   for_each  = local.k8s_map
 
-  cluster_name = local.k8s_map.key
-  cluster_location = local.k8s_map.value.location
+  cluster_name = each.key
+  cluster_location = each.location
 }
