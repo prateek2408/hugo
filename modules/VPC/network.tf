a8ab99d2b76393cd6085c1a64c25df1d4b8ad3fc
Prateek Khushalani <pkhushalani@google.com>
2021-10-04 22:07:38 +0530

    fixing var name

--- modules/VPC/network.tf
+++ modules/VPC/network.tf
@@ -2,5 +2,5 @@ resource "google_compute_network" "vpc" {
   count                   = var.create_vpc ? 1 : 0
   project                 = var.project
   name                    = var.vpc_name
-  auto_create_subnetworks = var.vpc_create_subnets
+  auto_create_subnetworks = var.create_subnets
 }
